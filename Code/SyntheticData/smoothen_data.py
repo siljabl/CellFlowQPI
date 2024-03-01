cf9e2476d6c062db0e3ae25a701d6aabac6a3ce0
silja uio <s.b.lastad@mn.uio.no>
2024-03-01 14:07:45 +0100

    Updating smoothing

--- Code/SyntheticData/smoothen_data.py
+++ Code/SyntheticData/smoothen_data.py
@@ -1,24 +1,26 @@
 import sys
 import scipy as sc
 import numpy as np
+
 import matplotlib.pyplot as plt
 
+sys.path.append("src")
+from functions import smoothen
 from PIL import Image
 
 # import rescaled and registered data
 input   = sys.argv[1]
-out_dir = sys.argv[2]
+out_dir = "../../Data/InitialConditions" #sys.argv[2]
 sigma = 2
 
 # smoothen
 im = plt.imread(input)
-smoothed_data = sc.ndimage.gaussian_filter(im, sigma=sigma)
+smoothed_data = smoothen(im)
 
 # save
 filename = input.split('/')[-1]
 im_tif  = Image.fromarray(smoothed_data.astype(np.uint8))
 im_tif.save(out_dir + filename)
 
-# glatte og legge på støy...
 # endre vekt mellom u og v
 # divergensfelt
