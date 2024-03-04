fb286dd6773c75f8ba83308564af30df6c107cb9
silja uio <s.b.lastad@mn.uio.no>
2024-03-04 15:53:32 +0100

    updating folder name

--- DIC/runSeveral.sh
+++ DIC/runSeveral.sh
@@ -7,12 +7,11 @@ SzW=( 1 2 5 10 20 50 )
 for field in "${fields[@]}"; do
 for u     in "${umax[@]}";   do
 # updating config file
-sed -i -e "2s#.*#../Data/SyntheticTestData/$field/u_max_$u#" config.txt
+sed -i -e "2s#.*#../Data/SyntheticTestData/$field/umax_$u#" config.txt
 
 for szw in "${SzW[@]}";
 do
-  #./runDIC.sh $szw
-  echo $szw
+  ./runDIC.sh $szw
 done
 
 done
